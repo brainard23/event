161d1fe59ee10c5193cab16a6cefbe7247dc8c39
brainard <brainard.ordona@fligno.com>
2024-04-29 20:05:31 +0800

    update form

--- app/(root)/events/[id]/page.tsx
+++ app/(root)/events/[id]/page.tsx
@@ -3,6 +3,7 @@
 import { getEventById, getRelatedEventsByCategory } from '@/lib/actions/event.actions'
 import { formatDateTime } from '@/lib/utils';
 import { SearchParamProps } from '@/types'
+import { Calendar, MapPin } from 'lucide-react';
 import Image from 'next/image';
 
 const EventDetails = async ({ params: { id }, searchParams }: SearchParamProps) => {
@@ -33,7 +34,7 @@ const EventDetails = async ({ params: { id }, searchParams }: SearchParamProps) 
             <div className="flex flex-col gap-3 sm:flex-row sm:items-center">
               <div className="flex gap-3">
                 <p className="p-bold-20 rounded-full bg-green-500/10 px-5 py-2 text-green-700">
-                  {event.isFree ? 'FREE' : `$${event.price}`}
+                  {event.isFree ? 'FREE' : `$ ${event.price}`}
                 </p>
                 <p className="p-medium-16 rounded-full bg-grey-500/10 px-4 py-2.5 text-grey-500">
                   {event.category.name}
@@ -51,7 +52,7 @@ const EventDetails = async ({ params: { id }, searchParams }: SearchParamProps) 
 
           <div className="flex flex-col gap-5">
             <div className='flex gap-2 md:gap-3'>
-              <Image src="/assets/icons/calendar.svg" alt="calendar" width={32} height={32} />
+              <Calendar width={32} height={32} className='my-auto text-cyan-800	'/>
               <div className="p-medium-16 lg:p-regular-20 flex flex-wrap items-center">
                 <p>
                   {formatDateTime(event.startDateTime).dateOnly} - {' '}
@@ -65,7 +66,7 @@ const EventDetails = async ({ params: { id }, searchParams }: SearchParamProps) 
             </div>
 
             <div className="p-regular-20 flex items-center gap-3">
-              <Image src="/assets/icons/location.svg" alt="location" width={32} height={32} />
+              <MapPin width={32} height={32} className='my-auto text-cyan-800	'/>
               <p className="p-medium-16 lg:p-regular-20">{event.location}</p>
             </div>
           </div>
